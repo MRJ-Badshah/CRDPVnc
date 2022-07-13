e851befb5eb37d76359197263eadfcc362a62f61
MRJ Badshah <69976020+MRJ-Badshah@users.noreply.github.com>
2022-07-13 13:43:06 +0530

    Update start.py
    Shortened done ^^

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import os
 
-cmd = 'apt update && apt upgrade && apt install --assume-yes -y ./chrome-remote-desktop_current_amd64.deb && apt install -y xfce4 desktop-base dbus-x11 xscreensaver xfce4-terminal firefox-esr geany vim-gtk3'
+cmd = 'apt update && apt upgrade && wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && apt install --assume-yes -y ./chrome-remote-desktop_current_amd64.deb && apt install -y xfce4 desktop-base dbus-x11 xscreensaver xfce4-terminal firefox-esr geany vim-gtk3'
 os.system(cmd)
 print("Desktop up. Thanks for using!")
